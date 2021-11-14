b14bf281fed8f7f9fbff885c6e1193aff99dda4d
Siaka11 <ouattarasiaka11@gmail.com>
2021-11-14 18:13:12 +0000

    first send

---
+++ Components/HeaderTabs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HeaderTabs = () => {
+    return (
+        <View>
+            <Text>Delivery</Text>
+            <Text>Pick up</Text>
+        </View>
+    )
+}
+
+co
+
+export default HeaderTabs
